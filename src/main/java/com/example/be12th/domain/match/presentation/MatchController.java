96c49851620348e8640793420bc594c425725f07
Kanghee <kh63167574@gmail.com>
2026-03-04 19:58:41 +0900

    feat : 경기 매치 상세조회 기능추가

--- src/main/java/com/example/be12th/domain/match/presentation/MatchController.java
+++ src/main/java/com/example/be12th/domain/match/presentation/MatchController.java
@@ -1,4 +1,22 @@
 package com.example.be12th.domain.match.presentation;
 
+import com.example.be12th.domain.match.presentation.dto.response.MatchResponse;
+import com.example.be12th.domain.match.service.MatchQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/match")
 public class MatchController {
+
+    private MatchQueryService matchQueryService;
+
+    @GetMapping("/match_id")
+    public MatchResponse query(@PathVariable Long matchId) {
+        return matchQueryService.execute(matchId);
+    }
 }
---
+++ src/main/java/com/example/be12th/domain/match/service/MatchQueryService.java
@@ -0,0 +1,22 @@
+package com.example.be12th.domain.match.service;
+
+import com.example.be12th.domain.match.domain.Match;
+import com.example.be12th.domain.match.domain.repository.MatchRepository;
+import com.example.be12th.domain.match.presentation.dto.response.MatchResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MatchQueryService {
+    private final MatchRepository matchRepository;
+
+    @Transactional(readOnly = true)
+    public MatchResponse execute(Long matchId){
+        Match match = matchRepository.findById(matchId)
+                .orElseThrow(() -> new RuntimeException("해당 매치를 찾을수 없습니다."));
+
+        return MatchResponse.from(match);
+    }
+}
