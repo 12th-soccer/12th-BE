2dbb453f32a7e2c98ddae508f080e81b768207b2
Kanghee <kh63167574@gmail.com>
2026-03-04 15:07:22 +0900

    feat : 환경설정

---
+++ BE-12th/src/main/java/com/example/be12th/Be12thApplication.java
@@ -0,0 +1,13 @@
+package com.example.be12th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be12thApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Be12thApplication.class, args);
+    }
+
+}
---
+++ BE-12th/src/test/java/com/example/be12th/Be12thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be12th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be12thApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
