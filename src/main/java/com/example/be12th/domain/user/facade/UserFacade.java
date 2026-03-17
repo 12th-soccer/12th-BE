a3374d59fb6869770e6b9bb2703e366a35716207
Kanghee <kh63167574@gmail.com>
2026-03-17 20:16:02 +0900

    feat :user facade 추가

---
+++ src/main/java/com/example/be12th/domain/user/facade/UserFacade.java
@@ -0,0 +1,21 @@
+package com.example.be12th.domain.user.facade;
+
+import com.example.be12th.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade{
+    private final UserRepository userRepository;
+
+    public Long currentUserId() {
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return userRepository.findByEmail(accountId)
+                .orElseThrow(() -> new UsernameNotFoundException("현재 유저를 찾을수없습니다.")).getId();
+    }
+}
+
