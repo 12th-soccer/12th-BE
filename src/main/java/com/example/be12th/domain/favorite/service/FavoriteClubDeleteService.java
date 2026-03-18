bbbcd595906653c4d755faa2facfb2d30a867804
Kanghee <kh63167574@gmail.com>
2026-03-18 17:05:22 +0900

    refactor : 사용자 소유 검증 추가

--- src/main/java/com/example/be12th/domain/favorite/service/FavoriteClubDeleteService.java
+++ src/main/java/com/example/be12th/domain/favorite/service/FavoriteClubDeleteService.java
@@ -2,6 +2,7 @@ package com.example.be12th.domain.favorite.service;
 
 import com.example.be12th.domain.favorite.domain.FavoriteClub;
 import com.example.be12th.domain.favorite.domain.repository.FavoriteClubRepository;
+import com.example.be12th.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,10 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class FavoriteClubDeleteService {
     private final FavoriteClubRepository favoriteClubRepository;
+    private final UserFacade userFacade;
 
     @Transactional
     public void execute(Long clubId){
-        FavoriteClub favoriteClub = favoriteClubRepository.findById(clubId)
+        FavoriteClub favoriteClub = favoriteClubRepository.findByUserIdAndClubId(userFacade.currentUserId(),clubId)
                 .orElseThrow(() -> new RuntimeException("해당 관심구단을 찾을수 없습니다."));
         favoriteClubRepository.delete(favoriteClub);
     }
