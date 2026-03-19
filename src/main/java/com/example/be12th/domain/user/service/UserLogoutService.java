c955081b569cadb1aab6dcd57bd1b490f4fe38a4
Kanghee <kh63167574@gmail.com>
2026-03-19 19:15:09 +0900

    refactor : RefreshRepository 를 통해 일관적인 Refreshtoken 삭제

--- src/main/java/com/example/be12th/domain/user/service/UserLogoutService.java
+++ src/main/java/com/example/be12th/domain/user/service/UserLogoutService.java
@@ -1,25 +1,23 @@
 package com.example.be12th.domain.user.service;
 
+import com.example.be12th.domain.auth.repository.RefreshTokenRepository;
 import com.example.be12th.global.jwt.JwtTokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserLogoutService {
     private final JwtTokenProvider jwtTokenProvider;
-    private final RedisTemplate<String, String> redisTemplate;
+    private final RefreshTokenRepository refreshTokenRepository;
 
-    @Transactional
     public void logout(HttpServletRequest httpServletRequest) {
         String accessToken = jwtTokenProvider.resolveToken(httpServletRequest);// HttpServletRequest 에서 받아온 클라이언트 정보중 토큰만 추출
 
-        if(accessToken != null || jwtTokenProvider.validateToken(accessToken)) {
+        if(accessToken != null && jwtTokenProvider.validateToken(accessToken)) {
             String email = jwtTokenProvider.getEmailFromToken(accessToken);
-            redisTemplate.delete(email);
+            refreshTokenRepository.deleteById(email);
         }
     }
 }
