45ea240205c1735f75242743aa5f0ce51f663eea
Kanghee <kh63167574@gmail.com>
2026-04-02 17:09:44 +0900

    feat : 골 관련 API추가

--- src/main/java/com/example/be12th/domain/goal/domain/Goal.java
+++ src/main/java/com/example/be12th/domain/goal/domain/Goal.java
@@ -3,18 +3,15 @@ package com.example.be12th.domain.goal.domain;
 import com.example.be12th.domain.match.domain.Match;
 import com.example.be12th.domain.player.domain.Player;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@Getter
 @Table(name = "tbl_goal")
-public class GoalEntity {
+public class Goal {
     @Id
     @Column(name = "goal_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,4 +28,6 @@ public class GoalEntity {
     @Column(name = "goal_time")
     private Integer goalTime;
 
+    @Column(name = "goal_type")
+    private GoalType goalType;
 }
--- src/main/java/com/example/be12th/domain/goal/domain/repository/GoalRepository.java
+++ src/main/java/com/example/be12th/domain/goal/domain/repository/GoalRepository.java
@@ -1,8 +1,11 @@
 package com.example.be12th.domain.goal.domain.repository;
 
-import com.example.be12th.domain.goal.domain.GoalEntity;
+import com.example.be12th.domain.goal.domain.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
-public interface GoalRepository extends JpaRepository<GoalEntity, Long> {
-    
+public interface GoalRepository extends JpaRepository<Goal, Long> {
+    @Query("SELECT g FROM Goal g JOIN FETCH g.match WHERE g.player.id = :playerId")
+    List<Goal> findByPlayerId(Long playerId);
 }
