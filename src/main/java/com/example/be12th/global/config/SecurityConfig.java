cd3a982bb918121879138a69e89cd55c5dd8248a
Kanghee <kh63167574@gmail.com>
2026-04-09 19:08:57 +0900

    refactor : security config 수정

--- src/main/java/com/example/be12th/global/config/SecurityConfig.java
+++ src/main/java/com/example/be12th/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.be12th.global.config;
 import com.example.be12th.domain.user.service.CustomOidcUserService;
 import com.example.be12th.domain.user.service.OAuth2LoginSuccessHandler;
 import com.example.be12th.global.jwt.JwtTokenFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,13 +27,51 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        //user
                         .requestMatchers(HttpMethod.POST, "/user/email").permitAll()
                         .requestMatchers(HttpMethod.POST, "/user/verify/signup").permitAll()
                         .requestMatchers(HttpMethod.POST, "/user/login").permitAll()
                         .requestMatchers("/", "/login/**", "/oauth2/**", "/user/login/success").permitAll()
-                        .anyRequest().authenticated()
+
+                        //ranking
+                        .requestMatchers(HttpMethod.GET, "/ranking").authenticated()
+
+                        //club
+                        .requestMatchers(HttpMethod.GET, "/club/{clubId}").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/club/favorite").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/club/search").authenticated()
+
+                        //match
+                        .requestMatchers(HttpMethod.GET, "/match/{matchId}").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/matches").authenticated()
+
+                        //player
+                        .requestMatchers(HttpMethod.GET, "/player/{playerId}").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/player/interest").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/player/search").authenticated()
+
+                        //favorite
+                        .requestMatchers(HttpMethod.POST, "/favorite/player/{playerId}").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/favorite/player/{playerId}").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/favorite/club/{clubId}").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/favorite/club/{clubId}").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/favorite/schedule/{clubId}").authenticated()
+
+                        //event
+                        .requestMatchers(HttpMethod.GET, "/goal/{playerId}").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/event/{matchId}").authenticated()
+
+                        .anyRequest().permitAll()
                 )
+
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) ->
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED)
+                        )
+                )
+
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+
                 .oauth2Login(oauth -> oauth
                         .userInfoEndpoint(userInfo -> userInfo
                                 .oidcUserService(customOidcUserService)
