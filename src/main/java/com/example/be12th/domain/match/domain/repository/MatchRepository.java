e97773649c75bf86cf317bc70d2819a2ce80e8b6
Kanghee <kh63167574@gmail.com>
2026-04-09 19:23:50 +0900

    refactor : 랭킹에서 k1 k2 분류

--- src/main/java/com/example/be12th/domain/match/domain/repository/MatchRepository.java
+++ src/main/java/com/example/be12th/domain/match/domain/repository/MatchRepository.java
@@ -1,5 +1,6 @@
 package com.example.be12th.domain.match.domain.repository;
 
+import com.example.be12th.domain.league.LeagueType;
 import com.example.be12th.domain.match.domain.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface MatchRepository extends JpaRepository<Match, Long> {
     List<Match> findByHomeClubIdOrAwayClubId(Long homeClubId, Long awayClubId);
     List<Match> findByMatchDateBetween(LocalDateTime start, LocalDateTime end);
+    List<Match> findByLeagueTypeOrderByMatchDateAsc(LeagueType leagueType);
 }
