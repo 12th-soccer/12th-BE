63d451382a2627b18080fe2e3a495920db2d2d63
Kanghee <kh63167574@gmail.com>
2026-04-14 17:02:27 +0900

    refactor : 이벤트에 imgurl추가 인증코드 이메일 중복확인

--- src/main/java/com/example/be12th/domain/matchevent/presentation/dto/MatchEventResponse.java
+++ src/main/java/com/example/be12th/domain/matchevent/presentation/dto/MatchEventResponse.java
@@ -1,6 +1,5 @@
 package com.example.be12th.domain.matchevent.presentation.dto;
 
-import com.example.be12th.domain.club.domain.Club;
 import com.example.be12th.domain.matchevent.domain.MatchEvent;
 import com.example.be12th.domain.matchevent.domain.MatchEventType;
 import lombok.AllArgsConstructor;
@@ -12,6 +11,7 @@ public class MatchEventResponse {
     private Long eventId;
     private Long clubId;
     private String playerName;
+    private String playerImageUrl;
     private Integer eventMinute;
     private MatchEventType matchEventType;
 
@@ -20,6 +20,7 @@ public class MatchEventResponse {
                 matchEvent.getId(),
                 matchEvent.getPlayer().getClub().getId(),
                 matchEvent.getPlayer().getName(),
+                matchEvent.getPlayer().getPlayerImageUrl(),
                 matchEvent.getEventMinute(),
                 matchEvent.getEventType()
         );
