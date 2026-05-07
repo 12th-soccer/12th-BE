a478a1310d0b60d0ab9909cab5b1af2a04dafeb5
Kanghee <kh63167574@gmail.com>
2026-05-07 19:21:27 +0900

    refactor : lazy 변경

--- src/main/java/com/example/be12th/domain/favorite/domain/FavoritePlayer.java
+++ src/main/java/com/example/be12th/domain/favorite/domain/FavoritePlayer.java
@@ -21,7 +21,7 @@ public class FavoritePlayer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
