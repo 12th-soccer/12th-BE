b50216979cb27b35c88660453908fa8f0e3094d7
Kanghee <kh63167574@gmail.com>
2026-05-07 19:28:04 +0900

    refactor : pr 반영

--- src/main/java/com/example/be12th/domain/favorite/domain/Repository/FavoritePlayerRepository.java
+++ src/main/java/com/example/be12th/domain/favorite/domain/Repository/FavoritePlayerRepository.java
@@ -1,7 +1,6 @@
 package com.example.be12th.domain.favorite.domain.Repository;
 
 import com.example.be12th.domain.favorite.domain.FavoritePlayer;
-import com.example.be12th.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +10,4 @@ public interface FavoritePlayerRepository extends JpaRepository<FavoritePlayer, 
     boolean existsByUserIdAndPlayerId(Long userId, Long playerId);
     Optional<FavoritePlayer> findByUserIdAndPlayerId(Long userId, Long playerId);
     List<FavoritePlayer> findAllByUserId(Long userId);
-
-    Long user(User user);
 }
