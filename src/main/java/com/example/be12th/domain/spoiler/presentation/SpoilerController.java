faae0afcf915572ac3ad73402840f6b50c36c31f
Kanghee <kh63167574@gmail.com>
2026-05-19 05:19:16 +0900

    feat : spoiler 컨트롤러 제작

--- src/main/java/com/example/be12th/domain/spoiler/presentation/SpoilerController.java
+++ src/main/java/com/example/be12th/domain/spoiler/presentation/SpoilerController.java
@@ -1,12 +1,26 @@
 package com.example.be12th.domain.spoiler.presentation;
 
+import com.example.be12th.domain.spoiler.presentation.dto.Request.SpoilerRequest;
+import com.example.be12th.domain.spoiler.presentation.dto.Response.SpoilerResponse;
+import com.example.be12th.domain.spoiler.service.SpoilerSettingService;
+import com.example.be12th.domain.spoiler.service.SpoilerUpdateService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/spoiler")
 public class SpoilerController {
+    private final SpoilerSettingService spoilerSettingService;
+    private final SpoilerUpdateService spoilerUpdateService;
 
+    @GetMapping
+    public SpoilerResponse execute(){
+        return spoilerSettingService.execute();
+    }
+
+    @PatchMapping
+    public SpoilerResponse execute(@RequestBody SpoilerRequest spoilerRequest){
+        return spoilerUpdateService.execute(spoilerRequest);
+    }
 }
