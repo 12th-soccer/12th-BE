fa449d212bca641eaa36df7accc2e0c79274844f
Kanghee <kh63167574@gmail.com>
2026-05-20 14:20:26 +0900

    feat:  notice 엔티티추가

---
+++ src/main/java/com/example/be12th/domain/notice/domain/Notice.java
@@ -0,0 +1,30 @@
+package com.example.be12th.domain.notice.domain;
+
+import com.example.be12th.domain.recruitment.domain.Recruitment;
+import com.example.be12th.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notice_id",nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recruitment_id",nullable = false)
+    private Recruitment recruitment;
+
+    @Column(nullable = false,name = "notice_description")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+}
